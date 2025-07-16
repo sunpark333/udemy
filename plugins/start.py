2aab30e599e195b8ef4f337b2597f2205f953421
Utkarsh <135024884+UTK253@users.noreply.github.com>
2025-07-16 21:32:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, User
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
 from config import API_ID, API_HASH, ERROR_MESSAGE
 from database.db import db
-from TechVJ.strings import HELP_TXT
+from plugins.strings import HELP_TXT
 
 class batch_temp(object):
     IS_BATCH = {}
