51a0237bb22f92932c98692e4dbfec1ea8212e35
sunpark333 <99256042+sunpark333@users.noreply.github.com>
2025-08-17 00:33:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,4 +18,8 @@ DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server e
 DB_NAME = os.environ.get("DB_NAME", "beastxsrcb")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True)) 
+
+#if you want to save your videos in a channel
+# config.py में जोड़ें
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", ""))
